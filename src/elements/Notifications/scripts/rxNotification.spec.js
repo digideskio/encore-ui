4050d2d88d3a65b269761912c00b9463de361c7d
khaziwallis <khaziwallis@gmail.com>
2016-07-21 23:38:22 +0530

    refactor(rxNotify): addressed review comments

--- src/elements/Notifications/scripts/rxNotification.spec.js
+++ src/elements/Notifications/scripts/rxNotification.spec.js
@@ -1,10 +1,10 @@
-describe('Directive: rxNotification', function () {
-
+describe('rxNotify', function () {
     var scope, compile, el2, notifySvc;
     var messageText1 = 'My Message 1';
     var defaultStack = 'page';
+
     beforeEach(function () {
-        module('encore.ui.elements');
+        module('encore.ui.rxNotify');
         module('templates/rxNotification.html');
 
         inject(function ($rootScope, $compile, rxNotify) {
@@ -12,35 +12,40 @@ describe('Directive: rxNotification', function () {
             compile = $compile;
             notifySvc = rxNotify;
         });
-        var rxNotificationTemplate = '<rx-notification type="warning">' + messageText1 + '</rx-notification>';
-        el2 = helpers.createDirective(rxNotificationTemplate, compile, scope);
     });
 
-    it('should have warning CSS class when setting type attribute to warning', function () {
-        // Find the second div and check for class. Why won't it take div.rx-notification?
-        var newEl = el2.find('div').eq(1);
-        expect(newEl.hasClass('notification-warning')).to.be.true;
-    });
+    describe('element: Notifications', function () {
+        beforeEach(function () {
+            var rxNotificationTemplate = '<rx-notification type="warning">' + messageText1 + '</rx-notification>';
+            el2 = helpers.createDirective(rxNotificationTemplate, compile, scope);
+        });
 
-    it('should contain the message', function () {
-        var newEl = el2.find('span').eq(1);
-        expect(newEl.text()).to.contain(messageText1);
-    });
+        it('should have warning CSS class when setting type attribute to warning', function () {
+            // Find the second div and check for class. Why won't it take div.rx-notification?
+            var newEl = el2.find('div').eq(1);
+            expect(newEl.hasClass('notification-warning')).to.be.true;
+        });
 
-    it('should add notification to stack and remove original element', function () {
-        var stackTemplate = '<div><rx-notification stack="page" type="info">' + messageText1 +
-                                '</rx-notification></div>';
+        it('should contain the message', function () {
+            var newEl = el2.find('span').eq(1);
+            expect(newEl.text()).to.contain(messageText1);
+        });
 
-        // Before compiling this notification, stack should be empty
-        expect(notifySvc.stacks[defaultStack].length).to.equal(0);
-        el2 = helpers.createDirective(stackTemplate, compile, scope);
+        it('should add notification to stack and remove original element', function () {
+            var stackTemplate = '<div><rx-notification stack="page" type="info">' + messageText1 +
+                                    '</rx-notification></div>';
 
-        // Stack should now have the contents of the notification directive
-        expect(notifySvc.stacks[defaultStack].length).to.equal(1);
-        expect(notifySvc.stacks[defaultStack][0].type).to.equal('info');
-        expect(notifySvc.stacks[defaultStack][0].text).to.contain(messageText1);
+            // Before compiling this notification, stack should be empty
+            expect(notifySvc.stacks[defaultStack].length).to.equal(0);
+            el2 = helpers.createDirective(stackTemplate, compile, scope);
 
-        // el2 is now <div></div> and it should have no children
-        expect(el2.children().length).to.equal(0);
+            // Stack should now have the contents of the notification directive
+            expect(notifySvc.stacks[defaultStack].length).to.equal(1);
+            expect(notifySvc.stacks[defaultStack][0].type).to.equal('info');
+            expect(notifySvc.stacks[defaultStack][0].text).to.contain(messageText1);
+
+            // el2 is now <div></div> and it should have no children
+            expect(el2.children().length).to.equal(0);
+        });
     });
 });
